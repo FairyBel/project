ac64fa1b602fae495b21aac86bacf0cb165a00bd
Irina <belyaevai86@gmail.com>
2022-10-28 10:51:04 +0300

    lessons

--- project/js/script.js
+++ project/js/script.js
@@ -44,3 +44,176 @@ const a = prompt('Один из последних просмотренных ф
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+/*
+      let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+let num=5;
+while (num<=10){
+    console.log(num);
+    num++;
+}      
+
+for(i=10; i<20; i++){
+    if(i===13){
+        break;
+    }
+    console.log(i);
+}
+
+for (i=2; i<=16; i++){
+    if(i%2===0){
+        continue;
+    }else{
+        console.log(i);
+        i++;
+    }
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let 
+
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+}
+
+console.log(arrayOfNumbers);
+return arrayOfNumbers;
+
+    
+for(i=10; i<20; i++){
+    if(i===13){
+        break;
+    }
+    console.log(i);
+}
+
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+for (let i=0; i<arr.length; i++){
+    result[i]=arr[i];
+}
+console.log(result)
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === 'string') {
+        data[i] = `${data[i]} - done`;
+    }
+}
+
+console.log(data);
+
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i]
+}
+console.log (result)
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)  */
